47ec2e4b7ec125c794ed43247a6620e38d34af76
Vladimir Vujosevic <v.vujosevic@meteovista.nl>
2017-11-16 18:01:07 +0100

    init

--- node_modules/fluxible/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright 2014, Yahoo! Inc.
- * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
- */
-var Fluxible = require('./lib/Fluxible');
-Fluxible.Fluxible = require('./lib/Fluxible');
-
-module.exports = Fluxible;
--- node_modules/fluxible/utils/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    createMockActionContext: require('./createMockActionContext'),
-    createMockComponentContext: require('./createMockComponentContext')
-};
