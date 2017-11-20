841e5f839f8fa5f151f2e52aeb245bc45fc6cc06
Vladimir Vujosevic <vladimirvuj@gmail.com>
2017-11-20 23:09:36 +0100

    init

--- app/server.js
+++ app/server.js
@@ -1,10 +1,13 @@
 import express from express;
 import import compression from "compression";
 import bodyParser from 'body-parser';
+import cookieParser from "cookie-parser";
 import path from 'path';
 import csurf from 'csurf';
 
 import app from './app';
+
+import handleServerRendering from "./server/handleServerRendering";
 
 const staticPath = path.resolve(__dirname, "../static");
 
@@ -15,6 +18,7 @@ export default function (callback) {
   server.set("port", process.env.PORT || 3000);
 
   server.use(bodyParser.json());
+  server.use(cookieParser());
   server.use(compression());
   server.use(csurf({ cookie: true }));
 
@@ -26,4 +30,17 @@ export default function (callback) {
   server.use(serveStatic(staticPath, {
     maxAge: 365 * 24 * 60 * 60
   }));
+
+  server.use(handleServerRendering);
+
+  // Generic server errors (e.g. not caught by components)
+  server.use((err, req, res, next) => {  // eslint-disable-line no-unused-vars
+    console.log("Error on request %s %s", req.method, req.url);
+    console.log(err);
+    console.log(err.stack);
+    res.status(500).send("Something bad hserverened");
+  });
+
+  // Finally, start the express server
+  return server.listen(server.get("port"), () => callback(server));
 }
---
+++ app/server/handleServerRendering.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import serialize from "serialize-javascript";
+
+import { navigateAction } from "fluxible-router";
+
+
+function renderApp(req, res, context, next) {
+
+}
+
+export default handleServerRendering() {
+
+}
