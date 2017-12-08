960fc2d6e5d00c8f64c882d7782eeff68074dcbb
Vladimir Vujosevic <v.vujosevic@meteovista.nl>
2017-12-08 18:42:10 +0100

    add stats for webpack builds

---
+++ webpack/stats.js
@@ -0,0 +1,13 @@
+module.exports = {
+  performance: true,
+  source: true,
+  warnings: true,
+  version: true,
+  modules: true,
+  errors: true,
+  errorDetails: true,
+  colors: true,
+  timings: true,
+  hash: true,
+  usedExports: true,
+};
